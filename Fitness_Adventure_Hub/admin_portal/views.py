4962e730c4dda6a01948875478b7d8bc65f7daf8
Sai Kumar Mudhiraj <68888124+Sayikumarr@users.noreply.github.com>
2023-10-23 14:06:55 +0530

    Revert "added middleware for custom template and also added maintenance mode"
    This reverts commit dabfc705f190400119793de5cd066e72383a246c.

--- Fitness_Adventure_Hub/admin_portal/views.py
+++ Fitness_Adventure_Hub/admin_portal/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 
 from .forms import ContactForm
 from .models import Configuration,Activity,BlogPost
@@ -32,15 +31,15 @@ def index(request):
     return render(request,'mainIndex.html',context)
 
 
-def contactForm(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request,'contactSuccess.html')
-    else:
-        form = ContactForm()
-    return render(request,'contactForm.html',{'form': form})
+# def contactForm(request):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return render(request,'contactSuccess.html')
+#     else:
+#         form = ContactForm()
+#     return render(request,'contactForm.html',{'form': form})
 
 
 from django.shortcuts import render, get_object_or_404
@@ -48,3 +47,34 @@ from django.shortcuts import render, get_object_or_404
 def blogpost(request,pk):
     blog = get_object_or_404(BlogPost, id=pk)
     return render(request,'blogpost.html',{'blog': blog})
+
+
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import send_mail
+
+def contactForm(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            # Send an HTML email
+            subject = 'New contact form submission'
+            from_email = 'contact@iamshareef.in'  # Replace with your email address
+            recipient_list = ['contact@iamshareef.in','masaikumar19@gmail.com',form.cleaned_data['email']]  # Replace with the recipient's email address
+
+            # Render the HTML content from the template
+            html_message = render_to_string('contact_email.html', {'form': form})
+
+            plain_message = strip_tags(html_message)
+            send_mail( subject, plain_message, from_email, recipient_list ,html_message=html_message)
+            # email = EmailMessage(subject=subject, body=plain_message, from_email=from_email, recipient_list=recipient_list,attachments=(html_message, 'text/html'))
+            # email.content_subtype = 'html'
+
+            # email.send()
+
+            return render(request, 'contactSuccess.html')
+    else:
+        form = ContactForm()
+    return render(request, 'contactForm.html', {'form': form})
